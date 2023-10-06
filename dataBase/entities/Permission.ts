75027a78e198573abc50484f000f4c26aa183030
firas <fkmkh11@gmail.com>
2023-10-06 14:17:12 +0300

    add permission entity

--- dataBase/entities/Permission.ts
+++ dataBase/entities/Permission.ts
@@ -1,8 +1,24 @@
+import { PrimaryGeneratedColumn } from "typeorm/browser";
+import { ManyToMany } from "typeorm/browser";
+import { Column } from "typeorm/browser";
 import { BaseEntity } from "typeorm/browser";
 import { Entity } from "typeorm/browser";
+import { User } from "./User.js";
 
 @Entity()
 export class Permission extends BaseEntity {
+    @PrimaryGeneratedColumn('uuid')
+    permissionId : string
 
-    
+    @Column({
+        type:'enum',
+        enum: ['remove_user', 'add_user', 'send_message'],
+        default:'send_message'
+    })
+    Name:'remove_user' | 'add_user' | 'send_message'
+
+
+    @ManyToMany(()=>User)
+    user:User
+
 }
