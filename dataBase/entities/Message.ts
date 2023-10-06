bb21f95c3c55328cb520a08ccf422d41a685a1e3
firas <fkmkh11@gmail.com>
2023-10-06 17:01:51 +0300

    some fixes

--- dataBase/entities/Message.ts
+++ dataBase/entities/Message.ts
@@ -9,9 +9,9 @@ import {
     JoinTable,
     ManyToMany
 } from "typeorm";
-
 import { User } from "./User.js";
 import { ChatRoom } from "./Chatroom.js";
+import { Relation } from "typeorm/browser";
 
 @Entity()
 export class Message extends BaseEntity {
@@ -24,17 +24,17 @@ export class Message extends BaseEntity {
     @Column({ nullable: false })
     Content: string;
 
-    @CreateDateColumn({ type: 'timestamp'})
+    @CreateDateColumn({ type: 'timestamp' })
     Timestamp: Date ;
 
     @Column()
     ChatRoomID: number
 
-    @Column('blob',{ nullable: true })
+    @Column('blob', { nullable: true })
     Attachment: Buffer
 
-   
+    @ManyToOne(() => User , user=>user.messages)
+    user: Relation<User>
 
 
-   
 }
--- dataBase/entities/UserMute.ts
+++ dataBase/entities/UserMute.ts
@@ -1,18 +1,20 @@
-import { Entity,BaseEntity,JoinColumn , PrimaryGeneratedColumn,ManyToOne,OneToOne, Column } from "typeorm";
+import { Entity,BaseEntity,JoinColumn , PrimaryGeneratedColumn,ManyToOne,OneToOne } from "typeorm";
 import { User } from "./User.js";
+import { Column } from "typeorm";
+import { Relation } from "typeorm";
 
 @Entity() 
 export class UserMute extends BaseEntity{
     @PrimaryGeneratedColumn('uuid')
     MuteId:string
 
-    @Column()
-    MuterId:string
+    @ManyToOne(()=>User , user=>user.UserId)
+    @JoinColumn()
+    MuterId:Relation<User>
 
 
-    @Column()
-    MutedId:string
-
+    @OneToOne (()=>User, user=>user.UserId)
+    MutedId: Relation<User>
 
 
 
