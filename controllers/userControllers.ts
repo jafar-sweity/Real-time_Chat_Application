b61c4709dc8ff718ef4087a5aa0bcb322b52ac29
firas <fkmkh11@gmail.com>
2023-10-07 03:51:56 +0300

    making some fixes but socket doesnt work

--- controllers/userControllers.ts
+++ controllers/userControllers.ts
@@ -1,74 +1,79 @@
-import express from 'express';
 import { User } from '../dataBase/entities/User.js';
-import  isEmail  from 'validator/lib/isEmail.js';
+import {isEmail} from 'class-validator';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-const registerUser = async (req: express.Request, res: express.Response,next:express.NextFunction) => {
 
-const {Username,email,password}=req.body
+// Registration Function
+export const registerUser = async (Username:string, email:string, password:string) => {
+  try {
+    // Validation checks
+    const regexEmail = /[@gmail.com|@yahoo.com|@hotmail.com|@live.com|@outlook.com|@cloud.com]$/;
 
-const regexEmail=/[@gmail.com|@yahoo.com|@hotmail.com|@live.com|@outlook.com|@cloud.com]$/
-if(!regexEmail.test(email)&&!isEmail.default(email)){
-    return res.status(400).json({msg:"invalid email"})
-}
+    if (!regexEmail.test(email) && !isEmail(email)) {
+      return { success: false, msg: 'Invalid email' };
+    }
 
-if(!Username||!email||!password){
-    return res.status(400).json({msg:"please enter all fields"})
-}
+    if (!Username || !email || !password) {
+      return { success: false, msg: 'Please enter all fields' };
+    }
 
-if(Username.length<3 && Username.length>20){
-    return res.status(400).json({msg:"username must be at least 3 characters"})
-}
+    if (Username.length < 3 || Username.length > 20) {
+      return { success: false, msg: 'Username must be between 3 and 20 characters' };
+    }
 
-const isExist = await User.findOne({where:{Email:email}});
-if(isExist){
-    res.status(400).json({msg:"user already exists"})
-}
+    const isExist = await User.findOne({ where: { Email: email } });
+    if (isExist) {
+      return { success: false, msg: 'User already exists' };
+    }
 
-if(password.length<8 && password.length>20){
-    return res.status(400).json({msg:"password must be at least 8 characters"})
-}
-const newUser = new User();
-newUser.Username=Username;
-newUser.Email=email;
-newUser.Password=password;
+    if (password.length < 8 || password.length > 20) {
+      return { success: false, msg: 'Password must be between 8 and 20 characters' };
+    }
 
-try {
+    const newUser = new User();
+    newUser.Username = Username;
+    newUser.Email = email;
+    newUser.Password = password;
+
     await newUser.save();
-    return res.status(201).json({msg:`user ${Username}created successfully`})
-}catch (error) {
-    console.log(error);
-    return res.status(500).json({msg:"internal server error"})
-}
+    return { success: true, msg: `User ${Username} created successfully` };
+  } catch (error) {
+    console.error(error);
+    return { success: false, msg: 'Internal server error' };
+  }
+};
 
-const loginUser = async (req: express.Request, res: express.Response) => {  
+
     
-    const [email,password]=req.body;
+    // Login Function
+    export const login = async (email: string, password: string, socket: any) => {
+        try {
+          if (!email || !password) {
+            return { success: false, msg: 'Please enter all fields' };
+          }
+      
+          const user = await User.findOne({ where: { Email: email } });
+          if (!user) {
+            return { success: false, msg: 'User does not exist' };
+          }
+      
+          // Compare the provided plaintext password with the stored encrypted password
+          const isMatch = await bcrypt.compare(password, user.Password);
+          if (!isMatch) {
+            return { success: false, msg: 'Invalid credentials' };
+          }
+      
+          // Notify other users that this user is online
+          socket.emit('userOnline', { userId: user.UserId });
+      
+          const token = jwt.sign({ id: user.UserId }, process.env.JWT_SECRET || '', {
+            expiresIn: '30m',
+          });
+      
+          return { success: true, token, user: { Username: user.Username } };
+        } catch (error) {
+          console.error(error);
+          return { success: false, msg: 'Internal server error' };
+        }
+      };
     
-    if(!email||!password){
-        return res.status(400).json({msg:"please enter all fields"})
-    }
-
-    const user = await User.findOne({where:{Email:email}});
-    if(!user){
-        return res.status(400).json({msg:"user does not exist"})
-    }
-
-    const isMatch = await bcrypt.compare(password,user.Password);
-    if(!isMatch){
-        return res.status(400).json({msg:"invalid credentials"})
-    }
-    const token = jwt.sign({id:user.UserId,user:Username},process.env.JWT_SECRET||'',{
-        expiresIn:"30m"
-    });
-
-    res.status(200).json({token,user:user.Username})
-
-
-
-}
-
-
-
-
-}
--- index.ts
+++ index.ts
@@ -1,10 +1,15 @@
 import express from 'express'
 import dataSource from './dataBase/dataSource.js';
+import register from './routes/register.js'
+import login from './routes/login.js'
 const app = express();
 
 app.use(express.json());
 
-const PORT = 3000;
+app.use('/user',register);
+app.use('/user',login);
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT,()=>{
     console.log(`hello from PORT ${PORT}`);
     dataSource.initializeDB()
