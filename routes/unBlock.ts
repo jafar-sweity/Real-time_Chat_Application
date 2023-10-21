0dd0fa214d2eee5b2e689d39e6a8f309a94830fc
firas <fkmkh11@gmail.com>
2023-10-22 00:49:33 +0300

    some fixes

---
+++ routes/unBlock.ts
@@ -0,0 +1,13 @@
+// import bodyParser from 'body-parser';
+// import cookieParser from 'cookie-parser';
+// import express from 'express';
+// import { UnblockUser } from '../controllers/BlockController.js';
+
+
+// const app = express.Router();
+
+
+
+// export default app.delete('/unBlock',UnblockUser);
+
+
